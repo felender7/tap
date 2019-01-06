83565f7b3573013797ecb7bce91802706252183f
Felender Hlungwani <felender7@gmail.com>
2019-01-06 19:38:46 +0200

    fixed verification erros

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,8 +11,8 @@ class ApplicationController < ActionController::Base
   protected
 
       def configure_permitted_parameters
-        devise_parameter_sanitizer.permit(:sign_up, keys: [:name,:profile_type, :phone])
-        devise_parameter_sanitizer.permit(:account_update, keys: [:name, :profile_type, :phone])
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:name,:profile_type, :phone,:varification])
+        devise_parameter_sanitizer.permit(:account_update, keys: [:name, :profile_type, :phone, :varification])
       end
 
       def get_document_number
