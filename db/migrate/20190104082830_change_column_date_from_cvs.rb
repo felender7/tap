22f2594633309680ba593f1e3e9680b98c6775ab
Felender Hlungwani <felender7@gmail.com>
2019-01-06 10:39:01 +0200

    fixed date column

--- db/migrate/20190104082830_change_column_date_from_cvs.rb
+++ db/migrate/20190104082830_change_column_date_from_cvs.rb
@@ -1,5 +1,5 @@
 class ChangeColumnDateFromCvs < ActiveRecord::Migration[5.2]
   def change
-    change_column :cvs, :date_of_birth, 'date USING CAST(column_name AS integer)'
+    change_column :cvs, :date_of_birth, 'date USING CAST(date_of_birth AS date)'
   end
 end
